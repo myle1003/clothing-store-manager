c59539b2591d42ae302a925f5b9137c3dd2860da
Minh Hoàng Võ <vominhhoang12401@gmail.com>
2022-11-13 23:50:19 +0700

    main-cart mobile

---
+++ mobile/H3MShop/Core/Detail/ViewModel/DetailViewModel.swift
@@ -0,0 +1,92 @@
+//
+//  DetaiViewModel.swift
+//  H3MShop
+//
+//  Created by Van Huy on 18/10/2022.
+//
+
+import Foundation
+import SwiftUI
+
+class DetailViewModel: ObservableObject {
+    
+    //Animation Property
+    @Published var startAnimation = false
+    @Published var shoesAnimation = false
+    
+    @Published var showBag = false
+    @Published var saveCart = false
+    @Published var addItemToCart = false
+    @Published var endAnimation = false
+    
+    @Published var productDetail = ProductDetail(product: Product(_id: "", name: "", rate: 0, status: "", slug: "", urlImage: [], sold: 0, id_cate: "", size: [], color: [], description: "", price: 0, sellDay: "", weight: 0, discount: 0),number: [],comment: [],rate: Rate())
+    
+    //Perform Animations
+    func performAnimation(){
+        withAnimation(.easeInOut(duration:0.8)){
+            shoesAnimation.toggle()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.35){
+            withAnimation(.easeInOut){
+                self.showBag.toggle()
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.75){
+            withAnimation(.easeInOut(duration:0.5)){
+                self.saveCart.toggle()
+            }
+        }
+        //0.35+ 0.75 = 1.25
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.1){
+            self.addItemToCart.toggle()
+        }
+        
+        //end Animation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.25){
+            withAnimation(.easeInOut(duration:0.5)){
+                self.endAnimation.toggle()
+            }
+        }
+    }
+    
+    func resetAll(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){ [self] in
+                
+            withAnimation {
+                self.startAnimation = false
+            }
+                endAnimation = false
+                addItemToCart = false
+                showBag = false
+                shoesAnimation = false
+                saveCart = false
+            
+        }
+    }
+    
+        func fetchProductDetail(slug: String) async throws {
+    
+            print(slug)
+            
+            let urlString = Constants.baseURL + Endpoints.productDetail + "/\(slug)"
+    
+            guard let url = URL(string: urlString) else {
+                throw httpError.badURL
+            }
+    
+            let responseProductDetail : ProductDetail = try await HttpClient.shared.fetch(url: url)
+    
+    
+            DispatchQueue.main.async{
+                self.productDetail = responseProductDetail
+            }
+            
+            
+        }
+    
+    
+}
+
+
---
+++ mobile/H3MShop/Core/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,99 @@
+//
+//  MainViewModel.swift
+//  H3MShop
+//
+//  Created by Van Huy on 22/10/2022.
+//
+
+import Foundation
+
+class MainViewModel: ObservableObject {
+    
+    @Published  var categories = [Category]()
+    @Published var products = [Product]()
+    
+    init(){
+        self.getProducts()
+        self.getCategories()
+    }
+    
+    func fetchCategories() async throws  -> [Category]{
+        
+        let urlString = Constants.baseURL + Endpoints.categories
+        
+        guard let url = URL(string: urlString) else {
+            throw httpError.badURL
+        }
+        
+        let categoriesResponse: [Category] = try await HttpClient.shared.fetch(url: url)
+        
+        return categoriesResponse
+
+    }
+    
+    func fetchProducts() async throws -> [Product]{
+        let urlString = Constants.baseURL + Endpoints.products
+        
+        guard let url = URL(string: urlString) else {
+            throw httpError.badURL
+        }
+        let productsResponse: [Product] = try await HttpClient.shared.fetch(url: url)
+        
+        return productsResponse
+    }
+    
+    func getProducts() {
+        Task{
+            do {
+                let existedProducts: [Product] = try await fetchProducts()
+                DispatchQueue.main.async{
+                    self.products = existedProducts
+                }
+            }
+            catch{
+                
+            }
+        }
+    }
+    
+    func getCategories() {
+        Task{
+            do {
+                let existedCategories: [Category] = try await fetchCategories()
+                DispatchQueue.main.async{
+                    self.categories = existedCategories
+                }
+            }
+            catch{
+                
+            }
+        }
+    }
+    
+    func fetchProductbyCategories(slug: String) async throws -> [Product]  {
+        let urlString = Constants.baseURL + Endpoints.productsbyCategory + "list/\(slug)/1"
+        print(urlString)
+        guard let url = URL(string: urlString) else {
+            throw httpError.badURL
+        }
+        
+        let productResponse: [Product] = try await HttpClient.shared.fetch(url: url)
+        
+        return productResponse
+    }
+    
+    func getProductbyCategory(slug: String) {
+        Task{
+            do{
+                let products = try await fetchProductbyCategories(slug: slug)
+                DispatchQueue.main.async {
+                    self.products = products
+                }
+            }
+            catch{
+                
+            }
+        }
+    }
+    
+}
