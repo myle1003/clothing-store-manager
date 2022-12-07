8da01e6a33f137508a48d7f4e42c44b450944edd
Nguyễn Trọng Nghĩa <trongnghia161001@gmail.com>
2022-12-07 19:00:49 +0700

    sentry

--- api/index.js
+++ api/index.js
@@ -1,3 +1,24 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://1e3288f0cf8c422996bab7418c4bbdac@o4504284649422849.ingest.sentry.io/4504285995532288",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test"
+});
+
 const Joi = require('joi');
 require('dotenv').config();
 var cors = require('cors')
