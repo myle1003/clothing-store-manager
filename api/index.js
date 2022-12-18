b7d7c90fa77f33449627398134f984c160affc33
My <myle10032001@gmail.com>
2022-12-18 16:21:13 +0700

    update code

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const Tracing = require("@sentry/tracing");
 // import * as Tracing from '@sentry/tracing';
 
 Sentry.init({
-  dsn: "https://52c17bbaca804360aaaa8025b14d8b9d@o4504288075055104.ingest.sentry.io/4504288173293568",
+  dsn: "https://6df9220f7c08425e8574dc80eccdd916@o4504284649422849.ingest.sentry.io/4504348937879552",
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
@@ -19,27 +19,17 @@ const transaction = Sentry.startTransaction({
   op: "test"
 });
 
-
 const Joi = require('joi');
 require('dotenv').config();
 var cors = require('cors')
 
 var express = require('express');
 var app = express();
-// let corsOptions = {
-//   origin : ['http://localhost:3000','http://127.0.0.1:4040','http://localhost:3002'],
-// }
 app.use(cors())
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+
 const mongoose = require('mongoose');
-// echo redis errors to the console
 const http = require('http').Server(app);
-// const cateAdmin = require('./Route/CategoryAdmin');
 const productAdmin = require('./Route/ProductAdmin');
 const categoryAdmin = require('./Route/CategoryAdmin');
 const stockAdmin = require('./Route/StockAdmin');
@@ -65,9 +55,9 @@ const {logger} = require('./logger/logger');
 logger.info('test');
 
 const upload = require('./Route/Upload');
-mongoose.connect('mongodb+srv://pnquang:quang123123a@cluster0.eenmlxn.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://pnquang:quang123a@cluster0.eenmlxn.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+    // .catch(err => console.error('Could not connect to MongoDB...'));
 app.use(express.json());
 app.use('/api/v1/cms/products', productAdmin);
 app.use('/api/v1/cms/categories', categoryAdmin);
